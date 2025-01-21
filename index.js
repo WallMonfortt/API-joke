61a44e821690e287306e3e98d5ef5c1653235b23
Wal Monfortt <jose.gual.monfortte.f@gmail.com>
2025-01-21 16:10:46 -0600

    get random joke to start page

--- index.js
+++ index.js
@@ -2,14 +2,36 @@ import express from 'express'
 import axios from 'axios'
 
 let PORT = 3000;
-let app = express()
+let app = express();
 
-const API_URL = " https://v2.jokeapi.dev/"
+const API_URL = " https://v2.jokeapi.dev"
 
-app.use(express.static('public'))
+let category = 'Any'
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+
+    axios.get(`${API_URL}/joke/${category}?type=single`).then(
+        function (response) {
+            console.log(response.data)
+            res.render('index.ejs', {
+                category: JSON.stringify(response.data.category),
+                joke: response.data.joke,
+                flags: JSON.stringify(response.data.flags)
+            } )
+        }
+    ).catch( function error(e) {
+        console.log(e)
+        res.render('index.ejs', {
+            content: JSON.stringify(error)
+        })
+    })
+})
 
 
 
 app.listen(PORT, () =>{
-    console.log(`Appliation listening on port: ${PORT}`)
+    console.log(`Appliation listening on port: ${PORT}`);
 })
